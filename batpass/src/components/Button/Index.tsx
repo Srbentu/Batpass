612699b6afe1bc6baa6436eb876c2e7ac7e744d3
Breno Marcomini <breno.marcomini@vitat.com.br>
2024-06-12 10:55:22 -0300

    lógica de copy

--- batpass/src/components/Button/Index.tsx
+++ batpass/src/components/Button/Index.tsx
@@ -3,6 +3,7 @@ import {Text, Pressable } from 'react-native';
 import styles from './Style';
 import Input from "../../components/Input/Index";
 import generatePass from "../../services/passwordService";
+import * as Clipboard from 'expo-clipboard';
 
 
 export default function BatButton() {
@@ -11,13 +12,16 @@ export default function BatButton() {
         let generateToken:string = generatePass()
         setPass(generateToken)
     }
+    function handleCopyButton(){
+        Clipboard.setStringAsync(pass)
+    }
     return (
         <>
             <Input pass={pass}/>
             <Pressable onPress={handleGenerateButton}>
                 <Text style={styles.button}>Generate Password</Text>
             </Pressable>
-            <Pressable onPress={() => console.log('salve')}>
+            <Pressable onPress={handleCopyButton}>
                 <Text style={styles.button}>Copy Code</Text>
             </Pressable>
         </>
